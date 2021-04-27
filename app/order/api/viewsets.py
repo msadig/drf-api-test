b117352257297d32ca0c8d9af583a8fdc885c4aa
Sadig Muradov <sadig@ymail.com>
2021-04-27 17:45:45 +0400

    filter orders by status and customer

--- app/order/api/viewsets.py
+++ app/order/api/viewsets.py
@@ -1,8 +1,12 @@
 from django.utils.translation import gettext_lazy as _
+from django.utils.decorators import method_decorator
 
 from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet
 from rest_framework.exceptions import NotAcceptable
+from rest_framework import filters
 from rest_framework_extensions.mixins import NestedViewSetMixin
+from drf_yasg import openapi
+from drf_yasg.utils import swagger_auto_schema
 
 
 from order.models import Order, Pizza, OrderItem
@@ -17,6 +21,21 @@ from .serializers import (
 from .mixins import MultiSerializerViewSetMixin
 
 
+@method_decorator(name='list', decorator=swagger_auto_schema(
+    manual_parameters=[
+        openapi.Parameter(
+            'status', openapi.IN_QUERY,
+            description=_("Filter orders by status"),
+            type=openapi.TYPE_STRING,
+            enum=[x.value for x in Order.DeliveryStatuses]
+        ),
+        openapi.Parameter(
+            'search', openapi.IN_QUERY,
+            description=_("Search for given customer's orders"),
+            type=openapi.TYPE_STRING
+        )
+    ]
+))
 class OrderViewSet(MultiSerializerViewSetMixin, NestedViewSetMixin, ModelViewSet):
     model = Order
     queryset = Order.objects.all()
@@ -27,6 +46,17 @@ class OrderViewSet(MultiSerializerViewSetMixin, NestedViewSetMixin, ModelViewSet
         "update": OrderUpdateSerializer,
         "partial_update": OrderUpdateSerializer,
     }
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['customer__full_name', 'customer__email']
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        status = self.request.query_params.get('status', None)
+
+        if status:
+            queryset = queryset.filter(status=status)
+
+        return queryset
 
     def update(self, request, *args, **kwargs):
         instance = self.get_object()
